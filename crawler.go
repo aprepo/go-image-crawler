9231296b877fea97c47f645594956af4daac8941
Ari-Pekka Repo <aprepo@gmail.com>
2024-10-20 12:18:10 +0300

    Refactor scanning to be a FileScanner object

--- crawler.go
+++ crawler.go
@@ -11,8 +11,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
 func callback(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		log.Fatal(err)
@@ -25,7 +23,6 @@ func callback(path string, info os.FileInfo, err error) error {
 		meta.DisplaySummary()
 		// TODO: Store to database
 	}
-	// fmt.Printf("dir: %v: name: %s\n", info.IsDir(), path)
 	return nil
 }
 
@@ -37,7 +34,11 @@ func setupDatabase() *sql.DB {
 	return db
 }
 
-func scanFiles(path string) {
+type FileScanner struct {
+	db *sql.DB
+}
+
+func (self FileScanner) scanFiles(path string) {
 	err := filepath.Walk(path, callback)
 	if err != nil {
 		log.Fatal(err)
@@ -49,8 +50,11 @@ func main() {
 	path := args[1]
 	fmt.Println("Search files from: " + path)
 
-	db = setupDatabase()
+	db := setupDatabase()
 	defer db.Close()
 
-	scanFiles(path)
+	scanner := FileScanner{
+		db: db,
+	}
+	scanner.scanFiles(path)
 }
